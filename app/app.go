867807b48fba61030a6b6de9ff2704d4e40088ef
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-07-01 01:28:27 -0300

    feat: initial configs for command ip

--- app/app.go
+++ app/app.go
@@ -7,5 +7,23 @@ func Generate() *cli.App {
 	app := cli.NewApp()
 	app.Name = "WebGetterCLI"
 	app.Usage = "Search for IP addresses and servers"
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Search for IP addresses on web",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "gooogle.com.br",
+				},
+			},
+			Action: searchIP,
+		},
+	}
+
 	return app
+}
+
+func searchIP(c *cli.Context) {
+
 }
