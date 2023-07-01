da4cdfe347d250a44f65149284207d794cc0a7d4
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-07-01 11:37:28 -0300

    feat: create function to search for servers

--- app/app.go
+++ app/app.go
@@ -13,17 +13,23 @@ func Generate() *cli.App {
 	app := cli.NewApp()
 	app.Name = "WebGetterCLI"
 	app.Usage = "Search for IP addresses and servers"
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name: "host",
+		},
+	}
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Search for IP addresses on web",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "gooogle.com.br",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Search for IP addresses on web",
+			Flags:  flags,
 			Action: searchIP,
+		},
+		{
+			Name:   "server",
+			Usage:  "Search for servers on web",
+			Flags:  flags,
+			Action: searchServer,
 		},
 	}
 
@@ -40,5 +46,17 @@ func searchIP(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func searchServer(c *cli.Context) {
+	host := c.String("host")
+
+	servers, err := net.LookupNS(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, server := range servers {
+		fmt.Println(server.Host)
 	}
 }
